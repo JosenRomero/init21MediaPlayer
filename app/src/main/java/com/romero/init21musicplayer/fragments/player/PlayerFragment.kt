333025218972360a607fb790a19d65d9f5eaf7d4
Josen Romero <romerojosemanuel@hotmail.com>
2021-12-02 19:08:46 -0600

    added play and pause song functionality

--- app/src/main/java/com/romero/init21musicplayer/fragments/player/PlayerFragment.kt
+++ app/src/main/java/com/romero/init21musicplayer/fragments/player/PlayerFragment.kt
@@ -1,11 +1,15 @@
 package com.romero.init21musicplayer.fragments.player
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.romero.init21musicplayer.R
 import com.romero.init21musicplayer.databinding.FragmentPlayerBinding
 
@@ -18,6 +22,14 @@ class PlayerFragment : Fragment() {
     // PlayerFragmentArgs - PlayerFragment
     private val args by navArgs<PlayerFragmentArgs>()
 
+    private var mediaPlayer: MediaPlayer? = null
+
+    lateinit var pathSong: String
+    lateinit var titleSong: String
+    lateinit var artUri: String
+
+    private var isPlaying: Boolean = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,8 +40,58 @@ class PlayerFragment : Fragment() {
         // if you need to understand the args.currentSong
         // see my_nav.xml argument and
         // see SongsAdapter.kt itemRowLayout.setOnClickListener
+        pathSong = args.currentSong.pathSong
+        titleSong = args.currentSong.titleSong
+        artUri = args.currentSong.artUri
+
+        setLayout()
+        initMediaPlayer()
+
+        binding.btnPlay.setOnClickListener {
+            if(isPlaying) pauseMusic()
+            else playMusic()
+        }
 
         return binding.root
+    }
+
+    private fun setLayout() {
+
+        // img
+        Glide.with(this)
+            .load(artUri)
+            .apply(RequestOptions())
+            .placeholder(R.drawable.ic_music_note)
+            .centerCrop()
+            .into(binding.imgSong)
+
+        binding.nameSong.text = titleSong
+
+    }
+
+    private fun initMediaPlayer() {
+
+        mediaPlayer = MediaPlayer()
+        mediaPlayer = MediaPlayer.create(requireContext(), Uri.parse(pathSong))
+
+        mediaPlayer!!.isLooping = true // loop
+        mediaPlayer!!.setVolume(0.5f, 0.5f) // volume
+        binding.timeBarSong.max = mediaPlayer!!.duration // time of the song
+
+        playMusic()
+
+    }
+
+    private fun playMusic() {
+        binding.btnPlay.setBackgroundResource(R.drawable.ic_pause)
+        isPlaying = true
+        mediaPlayer!!.start()
+    }
+
+    private fun pauseMusic() {
+        binding.btnPlay.setBackgroundResource(R.drawable.ic_play)
+        isPlaying = false
+        mediaPlayer!!.pause()
     }
 
 }
